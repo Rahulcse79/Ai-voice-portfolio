0d566293b9e4f097a9cc3bd2b7a0725bf697285e
Rahul singh <100202443+Rahulcse79@users.noreply.github.com>
2026-01-06 18:12:17 +0530

    fix

--- src/app/api/otp/send/route.ts
+++ src/app/api/otp/send/route.ts
@@ -82,15 +82,7 @@ export async function POST(req: NextRequest) {
   }
 }
 
-// app.get('/api/view', async (req, res) => {
-//   try {
-//     return res.status(200).json({ message: "view successful", status: 1 });
-//   } catch (error) {
-//     res.status(500).json({ message: 'Internal server error', error: error.message, status: -1 });
-//   }
-// });
-// /api/view
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
     return NextResponse.json(
       { message: "view successful", status: 1 },
